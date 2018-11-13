45ec5b0c0ab1dc3e9c7cbab6d1b406679d5d61d8
dingyongli <70761763@qq.com>
2018-11-13 18:51:10 +0800

    add imgs

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="Category">
-    这里是分类
+    <el-container>
+	  <el-header class="header">Header</el-header>
+	  <el-container>
+	    <el-aside style="width:15%;" class="aside">Aside</el-aside>
+	    <el-main class="main">Main</el-main>
+	  </el-container>
+	</el-container>
   </div>
 </template>
 
@@ -13,5 +19,8 @@ export default {
 <style scoped>
 .Shop{
 	text-align: center;
+}
+.header,.aside,.main{
+	border:1px solid red;
 }
 </style>
